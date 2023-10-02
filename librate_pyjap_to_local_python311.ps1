1d347f65fd4c3238993ab43c21714e8b6196a997
Askatect <146717559+Askatect@users.noreply.github.com>
2023-10-02 13:51:28 +0100

    Add files via upload

---
+++ librate_pyjap_to_local_python311.ps1
@@ -0,0 +1,7 @@
+﻿$location = Get-Location
+$location = ($location -split "\\")[0..2] -join "\"
+$location = (Get-ChildItem *Reference* -Path $location -Recurse -Directory)[0].FullName
+Set-Location $location
+$location = ($location -split "\\")[0..2] -join "\"
+$location += "\AppData\Local\Programs\Python\Python311\Lib\site-packages"
+copy-item -Path "pyjap" -Destination $location -Force
