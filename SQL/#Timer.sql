5378708a896f8453374777e0cb898aaa615fbc1d
Askatect <joshua.r.appleton@gmail.com>
2023-11-03 09:13:36 +0000

    The timer.sql procedure is now definitely not dynamic SQL.

--- SQL/#Timer.sql
+++ SQL/#Timer.sql
@@ -1,6 +1,6 @@
-CREATE PROCEDURE #Timer (
+CREATE PROCEDURE [dbo].[timer] (
 	@Start datetime,
-	@Process varchar(128) = ''Process'',
+	@Process varchar(128) = 'Process',
 	@End datetime = NULL
 )
 AS
@@ -10,10 +10,10 @@ DECLARE @Length int = DATEDIFF(second, @Start, @End)
 DECLARE @Hours int = @Length/3600,
 	@Minutes int = (@Length % 3600)/60,
 	@Seconds int = @Length % 60
-PRINT(CONCAT(@Process, '' started at '', FORMAT(@Start, ''HH:mm:ss''), '' and finished at '', FORMAT(@End, ''HH:mm:ss''), ''.''))
-PRINT(CONCAT(''This is a duration of '', 
-		IIF(@Hours = 0, '''', CONCAT(@Hours, '' hour'', IIF(@Hours = 1, '', '', ''s, ''))), 
-		IIF(@Minutes = 0, '''', CONCAT(@Minutes, '' minute'', IIF(@Minutes = 1, '' and '', ''s and ''))),
-			@Seconds, '' second'', IIF(@Seconds = 1, ''.'', ''s.''), CHAR(10)))
+PRINT(CONCAT(@Process, ' started at ', FORMAT(@Start, 'HH:mm:ss'), ' and finished at ', FORMAT(@End, 'HH:mm:ss'), '.'))
+PRINT(CONCAT('This is a duration of ', 
+		IIF(@Hours = 0, '', CONCAT(@Hours, ' hour', IIF(@Hours = 1, ', ', 's, '))), 
+		IIF(@Minutes = 0, '', CONCAT(@Minutes, ' minute', IIF(@Minutes = 1, ' and ', 's and '))),
+			@Seconds, ' second', IIF(@Seconds = 1, '.', 's.'), CHAR(10)))
 END
 GO
