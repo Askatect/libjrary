a82b26a2fbb78930fe1cdcde39a4fc4b633ac800
Askatect <146717559+Askatect@users.noreply.github.com>
2023-11-07 21:46:41 +0000

    Added indexes to build_database.sql.
    Also fixed issue with length of nchar and nvarchar types.

--- SQL/build_database.sql
+++ SQL/build_database.sql
@@ -14,7 +14,10 @@ END
 GO
 
 DECLARE @sql nvarchar(max) = '',
-      @schema nvarchar(max)
+	@schema nvarchar(max),
+	@table nvarchar(max),
+	@definition nvarchar(max),
+	@detail nvarchar(max)
 
 SET @sql = CONCAT('
 CREATE OR ALTER PROCEDURE [dbo].[build_', LOWER(DB_NAME()), '] (
@@ -85,22 +88,19 @@ END CATCH
 '
 
 --============================================================--
-/* Tables */
+/* Tables, Columns, Primary Keys, Unique Constraints, Check 
+Constraints and Default Constraints */
 
 SET @sql += '
 IF @print = 1
 	PRINT(''Building tables...'')
 '
 
-DECLARE @table nvarchar(max),
-	@cols nvarchar(max),
-	@constraints nvarchar(max)
-
 DECLARE tables_cursor cursor FAST_FORWARD FOR
 SELECT SCHEMA_NAME([t].[schema_id]) AS [schema],
 	[t].[name] AS [table],
 	STUFF(
-		(SELECT CONCAT(', ', CHAR(10), CHAR(9), '[', [c].[name], '] ', [d].[name], CASE WHEN [d].[system_type_id] IN (106, 108) THEN CONCAT('(', [d].[precision], ', ', [d].[scale], ')') WHEN [d].[system_type_id] IN (165, 167, 173, 175, 231, 239) THEN CONCAT('(', IIF([c].[max_length] = -1, 'max', CONVERT(varchar, [c].[max_length])), ')') ELSE '' END, IIF([c].[is_nullable] = 1, ' NULL', ' NOT NULL'), IIF([dc].[object_id] IS NOT NULL, CONCAT(' CONSTRAINT ', [dc].[name], ' DEFAULT ', REPLACE([dc].[definition], '''', '''''')), ''), IIF([cc].[is_disabled] = 0, CONCAT(' CONSTRAINT ', [cc].[name], ' CHECK ', REPLACE([cc].[definition], '''', '''''')), ''))
+		(SELECT CONCAT(', ', CHAR(10), CHAR(9), '[', [c].[name], '] ', [d].[name], CASE WHEN [d].[system_type_id] IN (106, 108) THEN CONCAT('(', [d].[precision], ', ', [d].[scale], ')') WHEN [d].[system_type_id] IN (165, 167, 173, 175, 231, 239) THEN CONCAT('(', IIF([c].[max_length] = -1, 'max', CONVERT(varchar, IIF([d].[system_type_id] IN (231, 239), [c].[max_length]/2, [c].[max_length]))), ')') ELSE '' END, IIF([c].[is_nullable] = 1, ' NULL', ' NOT NULL'), IIF([dc].[object_id] IS NOT NULL, CONCAT(' CONSTRAINT ', [dc].[name], ' DEFAULT ', REPLACE([dc].[definition], '''', '''''')), ''), IIF([cc].[is_disabled] = 0, CONCAT(' CONSTRAINT ', [cc].[name], ' CHECK ', REPLACE([cc].[definition], '''', '''''')), ''))
 		FROM sys.columns AS [c]
 			INNER JOIN sys.types AS [d]
 				ON [d].[system_type_id] = [c].[system_type_id]
@@ -125,11 +125,11 @@ OPEN tables_cursor
 FETCH NEXT FROM tables_cursor
 INTO @schema,
 	@table, 
-	@cols
+	@definition
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
-	SET @constraints = (
+	SET @detail = (
 		SELECT CONCAT(', ', CHAR(10), CHAR(9), 'CONSTRAINT ', [constraint], IIF([is_primary_key] = 1, ' PRIMARY KEY ', ' UNIQUE '), IIF([is_primary_key] = 1, [type] COLLATE database_default, ''), '(', [cols], ')')
 		FROM (
 			SELECT SCHEMA_NAME([t].[schema_id]) AS [schema],
@@ -161,12 +161,17 @@ BEGIN
 
 	SET @sql += CONCAT('
 IF @replace = 1
-	EXEC(''DROP TABLE IF EXISTS [', @schema, '].[', @table, ']'')
+BEGIN
+	SET @cmd = ''DROP TABLE IF EXISTS [', @schema, '].[', @table, ']''
+	IF @print = 1
+		PRINT(@cmd)
+	EXEC(@cmd)
+END
 IF (OBJECT_ID(''[', @schema, '].[', @table, ']'') IS NULL)
 BEGIN
 	SET @cmd = ''
 CREATE TABLE [', @schema, '].[', @table, '] (
-	', @cols, @constraints, '
+	', REPLACE(REPLACE(REPLACE(REPLACE(@definition, '&quot;', '"'), '&amp;', '&'), '&lt;', '<'), '&gt;', '>'), @detail, '
 )
 ''
 	IF @print = 1
@@ -178,43 +183,86 @@ END
 	FETCH NEXT FROM tables_cursor
 	INTO @schema,
 		@table, 
-		@cols
+		@definition
 END
 
 CLOSE tables_cursor
 DEALLOCATE tables_cursor
 
 --============================================================--
-/* Constraints */
+/* Indexes */
 
-SELECT SCHEMA_NAME([t].[schema_id]) AS [schema],
-	[t].[name] AS [table],
-	STUFF(
-		(SELECT CONCAT(', ', CHAR(10), CHAR(9), '[', [c].[name], '] ', [d].[name], CASE WHEN [d].[system_type_id] IN (106, 108) THEN CONCAT('(', [d].[precision], ', ', [d].[scale], ')') WHEN [d].[system_type_id] IN (165, 167, 173, 175, 231, 239) THEN CONCAT('(', IIF([d].[max_length] = -1, 'max', [d].[max_length]), ')') ELSE '' END, IIF([c].[is_nullable] = 1, ' NULL', ' NOT NULL'), IIF([dc].[object_id] IS NOT NULL, CONCAT(' CONSTRAINT ', [dc].[name], ' DEFAULT ', REPLACE([dc].[definition], '''', '''''')), ''), IIF([cc].[is_disabled] = 0, CONCAT(' CONSTRAINT ', [cc].[name], ' DEFAULT ', REPLACE([cc].[definition], '''', '''''')), ''))
-		FROM sys.columns AS [c]
-			INNER JOIN sys.types AS [d]
-				ON [d].[system_type_id] = [c].[system_type_id]
-				AND [d].[user_type_id] = [c].[user_type_id]
-			LEFT JOIN sys.default_constraints AS [dc]
-				ON [dc].[parent_object_id] = [c].[object_id]
-				AND [dc].[parent_column_id] = [c].[column_id]
-			LEFT JOIN sys.check_constraints AS [cc]
-				ON [cc].[parent_object_id] = [c].[object_id]
-				AND [cc].[parent_column_id] = [c].[column_id]
-		WHERE [c].[object_id] = [t].[object_id]
-		FOR XML PATH('')
-	), 1, 4, '') AS [cols]
-FROM sys.tables AS [t]
-GROUP BY [t].[schema_id],
-	[t].[object_id],
-	[t].[name]
-ORDER BY [t].[name]
+DECLARE index_cursor cursor FAST_FORWARD FOR
+SELECT [schema],
+	[table],
+	[index],
+	CONCAT('CREATE ', IIF([is_unique] = 1, 'UNIQUE ', ''), [type] COLLATE database_default, ' INDEX ', [index], ' ON [', [schema], '].[', [table], '] (', [cols], ')')
+FROM (
+	SELECT SCHEMA_NAME([t].[schema_id]) AS [schema],
+		[t].[name] AS [table],
+		STUFF((
+			SELECT CONCAT(', [', [c].[name], ']', IIF([ic].[is_descending_key] = 0, ' ASC', ' DESC'))
+			FROM sys.index_columns AS [ic]
+				INNER JOIN sys.columns AS [c]
+					ON [ic].[object_id] = [c].[object_id]
+					AND [ic].[column_id] = [c].[column_id]
+			WHERE [ic].[object_id] = [t].[object_id]
+				AND [ic].[index_id] = [i].[index_id]
+			FOR XML PATH('')
+		), 1, 2, '') AS [cols],
+		[i].[name] AS [index],
+		[i].[is_unique],
+		[i].[type_desc] AS [type]
+	FROM sys.tables AS [t]
+		INNER JOIN sys.indexes AS [i]
+			ON [i].[object_id] = [t].[object_id]
+	WHERE [i].[is_primary_key] = 0
+		AND [i].[is_unique_constraint] = 0
+		AND [i].[index_id] > 0
+) AS [T]
 
-SELECT OBJECT_NAME([parent_object_id]), *
-FROM sys.default_constraints AS [d]
-	LEFT JOIN sys.columns AS [c]
-		ON [c].[column_id] = [d].[parent_column_id]
-		AND [c].[object_id] = [d].[parent_object_id]
+OPEN index_cursor
+
+FETCH NEXT FROM index_cursor
+INTO @schema,
+	@table,
+	@detail,
+	@definition
+
+WHILE @@FETCH_STATUS = 0
+BEGIN
+	SET @sql += CONCAT('
+IF @replace = 1
+BEGIN
+	SET @cmd = ''DROP INDEX IF EXISTS ', @detail, ' ON [', @schema, '].[', @table, ']''
+	IF @print = 1
+		PRINT(@cmd)
+	EXEC(@cmd)
+END
+SET @cmd = ''', @definition, '''
+IF @print = 1
+	PRINT(@cmd)
+EXEC(@cmd)
+')
+	
+	FETCH NEXT FROM index_cursor
+	INTO @schema,
+		@table,
+		@detail,
+		@definition
+END
+
+CLOSE index_cursor
+DEALLOCATE index_cursor
+
+--============================================================--
+/* Foreign Keys */
+
+--============================================================--
+/* Triggers */
+
+--============================================================--
+/* Views */
 
 --============================================================--
 
