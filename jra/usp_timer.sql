5ef5d711c9c9ed8b9f1d3952cd06d1d9872feec0
Josh A <Josh.Appleton@euler.net>
2024-01-18 17:32:02 +0000

    Developing usp_timer v2.0.

--- jra/usp_timer.sql
+++ jra/usp_timer.sql
@@ -27,14 +27,54 @@ EXECUTE [jra].[usp_timer] @start, 'Script'
 */
 AS
 BEGIN
-SET @End = ISNULL(@End, GETDATE())
-DECLARE @Length int = DATEDIFF(second, @Start, @End)
-DECLARE @Hours int = @Length/3600,
-	@Minutes int = (@Length % 3600)/60,
-	@Seconds int = @Length % 60
-PRINT(CONCAT(@Process, ' started at ', FORMAT(@Start, 'HH:mm:ss'), ' and finished at ', FORMAT(@End, 'HH:mm:ss'), '.'))
-PRINT(CONCAT('This is a duration of ', 
-		IIF(@Hours = 0, '', CONCAT(@Hours, ' hour', IIF(@Hours = 1, ', ', 's, '))), 
-		IIF(@Minutes = 0, '', CONCAT(@Minutes, ' minute', IIF(@Minutes = 1, ' and ', 's and '))),
-			@Seconds, ' second', IIF(@Seconds = 1, '.', 's.'), CHAR(10)))
+DECLARE @process varchar(512) = NULL,
+	@start datetime = NULL,
+	@print bit = 1
+
+DECLARE @instance int
+
+WAITFOR DELAY '00:00:00'
+
+IF (OBJECT_ID('tempdb..#timer', 'U') IS NULL)
+BEGIN
+	SELECT [r].[session_id],
+		1 AS [instance_id], 
+		[r].[start_time] AS [task_start],
+		[r].[start_time] AS [instance_start],
+		GETDATE() AS [instance_end],
+		[s].[text] AS [batch_text],
+		[r].[sql_handle] AS [batch_id], 
+		[r].[plan_handle] AS [plan_id]
+	INTO #timer
+	FROM sys.dm_exec_requests AS [r]
+		CROSS APPLY sys.dm_exec_sql_text([r].[sql_handle]) AS [s]
+	WHERE [r].[session_id] = @@SPID
+	SET @instance = 1
+END
+ELSE
+BEGIN
+	SET @instance = (SELECT COUNT([instance_id]) + 1 FROM #timer)
+	INSERT INTO #timer([session_id], [instance_id], [task_start], [instance_start], [instance_end], [batch_text], [batch_id], [plan_id])
+	SELECT [r].[session_id], 
+		@instance,		
+		[r].[start_time],
+		COALESCE(@start, (SELECT [instance_end] FROM #timer WHERE [task_start] = [r].[start_time] AND [instance_id] = @instance - 1), [r].[start_time]),
+		GETDATE(),
+		[s].[text],
+		[r].[sql_handle], 
+		[r].[plan_handle]
+	FROM sys.dm_exec_requests AS [r]
+		CROSS APPLY sys.dm_exec_sql_text([r].[sql_handle]) AS [s]
+	WHERE [r].[session_id] = @@SPID
+END
+
+IF @print = 1
+BEGIN
+	DECLARE @statement nvarchar(max),
+		@duration bigint = (SELECT DATEDIFF(millisecond, [instance_start], [instance_end]) FROM #timer WHERE [instance_id] = @instance)
+	SET @statement = CONCAT(@process, ' (Instance ', @instance, ') - ', FORMAT((@duration/360000), '#00'), ':', FORMAT((@duration/60000 % 60), 'D2'), ':', FORMAT((@duration/1000 % 60), 'D2'), '.', FORMAT((@duration % 1000), 'D3'))
+	PRINT(LTRIM(@statement))
+END
+
+SELECT * FROM #timer
 END
